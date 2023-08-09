3bc037835d97d9bbe9b4cf94d8597e7f05e997ff
bavaaram <itsjustabavaar@gmail.com>
2023-08-09 19:57:17 +0330

    Add task model in tasks/models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(_("Title"), max_length=255)
+    description = models.TextField(_("Description"))
+    start_date = models.DateTimeField(_("Start Date"), auto_now=True, null=True)
+    end_date = models.DateTimeField(_("End Date"), auto_now=True, null=True)
+    deadline = models.DateTimeField(_("Dead Line"), auto_now=True)
+    sprint = models.ForeignKey("projects.Sprint", verbose_name=_("Sprint ID"), on_delete=models.CASCADE)
+    user = models.ForeignKey("accounts.User", verbose_name=_("User"), null=True, on_delete=models.SET_NULL)
+    status = models.ForeignKey("Status", verbose_name=_("Status"), null=True, on_delete=models.SET_NULL)
+
