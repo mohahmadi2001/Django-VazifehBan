7c0a34b77300d0e7f5af84044edbcc3e40b0882a
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-08-10 12:15:28 +0330

    add team to workspace

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,11 @@ from django.utils.translation import gettext_lazy as _
 
 class WorkSpace(models.Model):
     title = models.CharField(_("Title"), max_length=50)
-
+    team = models.ForeignKey("accounts.Team",
+                             verbose_name=_("Team"),
+                             on_delete=models.CASCADE
+                             )
+    
     class Meta:
         verbose_name = _("WorkSpace")
         verbose_name_plural = _("WorkSpaces")
