a4946b79041835cd46dedd6c987a43a81fad17ab
Mohammad Mahdi Ahmadi <moh.ahmdi2001@gmail.com>
2023-08-11 14:59:34 +0330

    soft delete

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.query import QuerySet
 # Create your models here.
 
+
 class MyManager(models.Manager):
     def get_queryset(self) -> QuerySet:
         return super().get_queryset().filter(is_deleted=False)
