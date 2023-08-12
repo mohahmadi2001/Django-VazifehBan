cf4088ff02bae689b54855bf7f8904a43f6790c7
bavaaram <itsjustabavaar@gmail.com>
2023-08-12 17:06:31 +0330

    Register accounts app models in admin panel in accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import CustomUser, Team, UserTeam
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserTeam)
+class UserTeamAdmin(admin.ModelAdmin):
+    pass
+
