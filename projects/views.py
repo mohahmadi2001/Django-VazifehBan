54f813fadd4ef40787895066a4e3f2f118893b7e
amiiirm8 <amirmahdimanafi8@gmail.com>
2023-08-17 20:29:53 +0330

    feat:create workspace edit view

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -7,6 +8,7 @@ from rest_framework.exceptions import PermissionDenied
 import logging
 
 from accounts.models import Team
+from projects.models import WorkSpace
 from .serializers import WorkSpaceSerializer
 from .permissions import IsTeamMember, IsTeamOwner
 
@@ -79,3 +81,71 @@ class WorkSpaceCreateView(CreateAPIView):
         """
         logger = logging.getLogger(__name__)
         logger.info(f"Workspace created: {workspace.title}, Team: {workspace.team.name}")
+
+
+class WorkSpaceEditView(RetrieveUpdateAPIView):
+    """
+    View for editing workspace information by user permission.
+
+    **Arguments:**
+        team_pk (int): The ID of the team that the workspace belongs to.
+        workspace_pk (int): The ID of the workspace to be edited.
+
+    **Permissions:**
+        IsAuthenticated: The user must be authenticated.
+        IsTeamMember: The user must be a member of the team that the workspace belongs to.
+
+    **Returns:**
+        A response object with the updated workspace information.
+
+    **Raises:**
+        PermissionDenied: If the user does not have permission to edit the workspace.
+
+    """
+
+    serializer_class = WorkSpaceSerializer
+    permission_classes = [IsAuthenticated, IsTeamMember]
+
+    def get_queryset(self):
+        """
+        Gets the queryset for the workspaces that the user can edit.
+
+        **Returns:**
+            The queryset for the workspaces that the user can edit.
+        """
+        team_pk = self.kwargs.get('team_pk')
+        workspace_pk = self.kwargs.get('workspace_pk')
+        return WorkSpace.objects.filter(team__pk=team_pk, pk=workspace_pk)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Updates the workspace information.
+
+        **Arguments:**
+            request (django.http.HttpRequest): The HTTP request.
+
+        **Raises:**
+            PermissionDenied: If the user does not have permission to edit the workspace.
+
+        **Returns:**
+            A response object with the updated workspace information.
+        """
+        workspace = self.get_object()
+        serializer = self.get_serializer(workspace, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        if not workspace.team.is_member(request.user):
+            raise PermissionDenied("You are not a member of this team.")
+
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def log_workspace_update(self, workspace):
+        """
+        Logs the editing of a workspace.
+
+        Args:
+            workspace: The newly edited workspace object.
+        """
+        logger = logging.getLogger(__name__)
+        logger.info(f"Workspace updated: {workspace.title}, Team: {workspace.team.name}")
