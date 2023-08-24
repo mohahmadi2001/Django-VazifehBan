7b994a7f4f4f3afa360978d0d95e677233413c71
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-08-24 16:18:58 +0330

    login and register api

--- VazifeBan/settings.py
+++ VazifeBan/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
+    'rest_framework_simplejwt',
     'accounts',
     'projects',
     'tasks',
@@ -54,6 +55,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=60),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': datetime.timedelta(days=1),  
+    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=1),  
+    'SLIDING_TOKEN_LIFETIME': datetime.timedelta(days=1),  
+    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=1),  
+    'SLIDING_TOKEN_LIFETIME': datetime.timedelta(days=1),
+}
 
 ROOT_URLCONF = 'VazifeBan.urls'
 
@@ -150,18 +162,9 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 
 REST_FRAMEWORK = {
-    'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 
-#djoser
-
-DJOSER = {
-    'LOGIN_FIELD': 'username',  
-    'USER_CREATE_PASSWORD_RETYPE': True,  
-    'SERIALIZERS': {
-        'user_create': 'path.to.your.custom.serializers.UserCreateSerializer',  
-        'user': 'path.to.your.custom.serializers.UserSerializer',    
-        'current_user': 'djoser.serializers.UserSerializer',     
-    },
-}
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-import logging
-# Create your views here.
+from .serializers import MyTokenObtainPairSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from .models import CustomUser
+
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+    
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
