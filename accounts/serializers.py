2ede9e9ca21ef90163b7537ff39fe100b5b4399e
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-08-24 10:18:32 +0330

    add serializer for create user

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,3 +1,13 @@
+from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
+from .models import CustomUser
 
-#create your serializers here
+class CustomUserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = CustomUser
+        fields = ('id', 'email', 'username', 'password', 'first_name', 'last_name')
+
+class CustomUserSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        model = CustomUser
+        fields = ('id', 'email', 'username', 'first_name', 'last_name')
